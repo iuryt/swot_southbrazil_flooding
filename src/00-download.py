ff8201d9bc6c751050db6b7f6f5fbb7954b3178e
iury simoes-sousa <simoesiury@gmail.com>
2024-06-26 16:19:15 -0400

    download pixel_cloud data

--- src/00-download.py
+++ src/00-download.py
@@ -3,8 +3,33 @@ from earthaccess import Auth, Store
 import xarray as xr
 import numpy as np
 import os
+from glob import glob
+from datetime import datetime
+import pyproj
+from tqdm import tqdm
 
-from tools import xlim, ylim
+from tools import download_and_extract_shapefile
+from tools import lonlim, latlim
+
+
+wgs84 = pyproj.CRS("EPSG:4326") # Geographic coordinates (WGS84)
+utm22s = pyproj.CRS("EPSG:32722")  # UTM zone 22S
+transformer = pyproj.Transformer.from_crs(wgs84, utm22s, always_xy=True)
+xlim, ylim = transformer.transform(lonlim, latlim)
+
+
+urls = [
+    "https://urbanismodrive.procempa.com.br/geopmpa/SPM/PUBLICO/PDDUA_ATUAL/SHP/Bairros_LC12112_16.zip",
+    "https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2022/UFs/RS/RS_Municipios_2022.zip",
+    "https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2022/UFs/RS/RS_RG_Imediatas_2022.zip",
+    "https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2022/UFs/RS/RS_RG_Intermediarias_2022.zip"
+]
+for url in urls:
+    download_and_extract_shapefile(url) 
+
+
+    
+    
 
 # Initialize and attempt login
 auth = Auth()
@@ -13,62 +38,61 @@ auth.login(strategy="netrc")
 # Define datasets of interest with their identifiers
 datasets = {
     "swot": {
-        "id": {"short_name": "SWOT_L2_HR_Raster_2.0"},
+        "id": {"doi": "10.5067/SWOT-PIXC-2.0"},
+    },
+    "imerg": {
+        "id": {"doi": "10.5067/GPM/IMERG/3B-HH-E/06"}
     },
 }
 
 # Define the time range for data acquisition
-time_ranges = {
-    "before": ("2024-04-10", "2024-04-26"),
-    "after": ("2024-04-28", "2024-05-14"),
-}
+time_range = ("2024-04-10", "2024-05-24")
 
-for time_key in time_ranges:
-    print(f"\n\n{time_key}\n")
-    time_range = time_ranges[time_key]
-    
-    # Loop through each dataset of interest
-    for key in datasets:
-        print(f"{key}\n")
-        
-        path = f"../data/external/{key}/"
-        # Create a directory for the current dataset if it doesn't exist
 
-        if not os.path.exists(path):
-            os.mkdir(path)
-        
-        path = f"{path}/{time_key}"
+# Loop through each dataset of interest
+for key in datasets:
+    print(f"{key}\n")
 
-        if not os.path.exists(path):
-            os.mkdir(path)
-            
-        # Search for data matching the dataset identifier and specified criteria
-        results = earthaccess.search_data(
-            **datasets[key]["id"],
-            cloud_hosted = True,
-            temporal = time_range,
-            granule_name = '*_100m_*',
-        )
+    path = f"../data/external/{key}/"
+    # Create a directory for the current dataset if it doesn't exist
 
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
+    # Search for data matching the dataset identifier and specified criteria
+    results = earthaccess.search_data(
+        **datasets[key]["id"],
+        cloud_hosted = True,
+        temporal = time_range,
+    )
+
+    if key == "swot":
         selected_results = []
-        for item in results:
+        for item in tqdm(results):
             spatial_domain = item["umm"]["SpatialExtent"]["HorizontalSpatialDomain"]
-            if "GPolygons" in list(spatial_domain["Geometry"]):
-                points = spatial_domain["Geometry"]["GPolygons"][0]["Boundary"]["Points"]
+            if "BoundingRectangles" in list(spatial_domain["Geometry"]):
+                for bounds in spatial_domain["Geometry"]["BoundingRectangles"]:
+                    lons = [bounds["WestBoundingCoordinate"], bounds["EastBoundingCoordinate"]]
+                    lats = [bounds["SouthBoundingCoordinate"], bounds["NorthBoundingCoordinate"]]
+                    
+                    for lon, lat in zip(lons, lats):
+                        isin = (
+                            (lon > lonlim[0]) & (lon < lonlim[1]) &
+                            (lat > latlim[0]) & (lat < latlim[1])
+                        )
+                        if isin:
+                            selected_results.append(item)
+    else:
+        selected_results = results
 
-                for point in points:
-                    lon = point["Longitude"]
-                    lat = point["Latitude"]
-                    isin = (
-                        (lon > xlim[0]) & (lon < xlim[1]) &
-                        (lat > ylim[0]) & (lat < ylim[1])
-                    )
-                    if isin:
-                        selected_results.append(item)
+    # Initialize a store object for data retrieval
+    store = Store(auth)
 
+    # Download data files based on the search results and store them in the specified path
+    files = store.get(selected_results, path)
+        
+        
 
-        # Initialize a store object for data retrieval
-        store = Store(auth)
+        
 
-        # Download data files based on the search results and store them in the specified path
-        files = store.get(selected_results, path)
